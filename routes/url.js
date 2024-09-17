aa5b281264f39ff68a33cb1bb8f4525b45250ee3
Varsha Mewada <varshamewada105@gmail.com>
2024-09-17 16:24:12 +0530

    Add files via upload

--- routes/url.js
+++ routes/url.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const router = express.Router();
 
+const {
+  handleGenerateShortURL,
+  handleGetShortURL,
+  handleAnalytics,
+} = require("../controllers/url");
+
+router.route("/").post(handleGenerateShortURL);
+router.route("/:shortId").get(handleGetShortURL);
+router.route("/analytics/:shortId").get(handleAnalytics);
+
+// or
+
+// router.post("/", handleGenerateShortURL);
+// router.get("/:shortId", handleGetShortURL);
+// router.get("/analytics/:shortId", handleAnalytics);
+
+module.exports = router;
